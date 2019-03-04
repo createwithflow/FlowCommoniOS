22d17fe1e0eeb50f9b047ffb0e731ebacd34eec0
Jeremie Benhamron <jeremiebenhamron@gmail.com>
2019-03-04 11:45:20 -0800

    fix year ranges in file headers

--- FlowCommon/Resources/Timeline.swift
+++ FlowCommon/Resources/Timeline.swift
@@ -1,4 +1,4 @@
-// Copyright © 2016-19 JABT Labs Inc.
+// Copyright © 2016-2019 JABT Labs Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to
